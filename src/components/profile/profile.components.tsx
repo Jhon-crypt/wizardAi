4a9b2abcf089b1901a7c2cb5e93bf9cfa213a09d
Jhon-crypt <johnoladele690@gmail.com>
2023-03-24 15:46:53 +0100

    Added settings modal

--- src/components/profile/profile.components.tsx
+++ src/components/profile/profile.components.tsx
@@ -5,15 +5,24 @@ import {
     Heading,
     VStack,
     Text,
-    Divider,
     HStack,
-    Button
+    Button,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton
 } from '@chakra-ui/react'
 import { Icon } from '@chakra-ui/react'
 import { BiUserPin, BiUserCircle, BiCog, BiLogOutCircle } from "react-icons/bi";
 
 
 function Profile() {
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     return (
 
@@ -103,7 +112,7 @@ function Profile() {
                                                 borderColor: '#5279F4',
                                                 backgroundColor: '#191919',
                                                 color: '#5279F4'
-                                            }}>
+                                            }} onClick={onOpen}>
 
                                             Settings
 
@@ -118,11 +127,11 @@ function Profile() {
                                     <Center>
 
                                         <Button leftIcon={<Icon as={BiLogOutCircle} />} bg='red' borderRadius={45} border='1px' borderColor='red'
-                                        _hover={{
-                                            borderColor: 'red',
-                                            backgroundColor: '#191919',
-                                            color: 'red'
-                                        }}>
+                                            _hover={{
+                                                borderColor: 'red',
+                                                backgroundColor: '#191919',
+                                                color: 'red'
+                                            }}>
 
                                             Logout
 
@@ -141,6 +150,26 @@ function Profile() {
                 </VStack>
 
             </Container>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent bg='#191919' color='white'>
+                    <ModalHeader> <Icon as={BiCog}/> Settings</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt aute id consequat veniam incididunt duis in sint irure nisi. Mollit officia cillum Lorem ullamco minim nostrud elit officia tempor esse quis.
+
+                        Sunt ad dolore quis aute consequat. Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
 
         </>
 
