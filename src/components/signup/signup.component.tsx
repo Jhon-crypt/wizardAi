9779d6f39404fbdbd7dd189c288bc9238832efd0
Jhon-crypt <johnoladele690@gmail.com>
2023-03-26 20:34:34 +0100

    Added signup function

--- src/components/signup/signup.component.tsx
+++ src/components/signup/signup.component.tsx
@@ -10,7 +10,8 @@ import {
     AlertIcon,
     CloseButton,
     Box,
-    AlertDescription
+    AlertDescription,
+    useDisclosure
 } from '@chakra-ui/react'
 import { Icon } from '@chakra-ui/icons';
 import { BiUserCircle } from "react-icons/bi";
@@ -18,30 +19,68 @@ import { BiUser } from "react-icons/bi";
 import { EmailIcon } from '@chakra-ui/icons';
 import { LockIcon } from '@chakra-ui/icons'
 import { useState } from 'react'
-import handleSignup from '../../api/auth/signupAuth';
+import supabase from '../../supabase/supabase';
 
 function Signup() {
 
-    const [FullName, setFullName] =  useState('')
+    const [FullName, setFullName] = useState('')
 
     const [Email, setEmail] = useState('');
 
     const [Password, setPassword] = useState('')
 
-    const signUpData = (event: any) => {
+    const [loading, setLoading] = useState(false);
+
+    const [error, setError] = useState(false)
+
+    const [signupStatus, setSignupStatus] = useState(false)
+
+    const signUpData = async (event: any) => {
+
+        setLoading(true)
 
         event.preventDefault();
 
-        try{
+        try {
 
-            handleSignup(Email, FullName, Password)
+            const { data, error } = await supabase.auth.signUp({
+                email: `${Email}`,
+                password: `${Password}`,
+                options: {
+                    data: {
 
-        }catch(error){
+                        full_name: `${FullName}`,
+
+                    }
+                }
+            })
+
+            if (error) {
+
+                console.log(error)
+
+                setError(true)
+
+                setSignupStatus(false)
+
+            } else {
+
+                setError(false)
+
+                setSignupStatus(true)
+
+                console.log(data)
+
+            }
+
+            setLoading(false)
+
+        } catch (error) {
 
             console.log(error)
 
         }
-    
+
     }
 
     return (
@@ -61,45 +100,98 @@ function Signup() {
                 <form onSubmit={signUpData}>
 
                     <Center>
-                        <Alert status='success' mb={3} color="black" as="b" borderRadius={45} w={410}>
-                            <AlertIcon />
-                            <Box>
-                                <AlertDescription>
-                                    Data uploaded to the server. Fire on!
-                                </AlertDescription>
-                            </Box>
-                            <CloseButton
-                                alignSelf='flex-start'
-                                position='relative'
-                                right={-10}
-                                top={-1}
-                            />
-                        </Alert>
+                        {error ?
+
+                            (
+
+
+
+                                <Alert status='error' mb={3} color="black" as="b" borderRadius={45} w={300}>
+                                    <AlertIcon />
+                                    <Box>
+                                        <AlertDescription>
+                                            <Center>
+                                                Something went wrong
+                                            </Center>
+                                        </AlertDescription>
+                                    </Box>
+                                </Alert>
+
+                            ) : (
+
+                                <></>
+
+                            )
+
+                        }
+
+                        {signupStatus ?
+
+                            (
+
+                                <Alert status='success' mb={3} color="black" as="b" borderRadius={45} w={350}>
+                                    <AlertIcon />
+                                    <Box>
+                                        <AlertDescription>
+                                            Account created, check your mail
+                                        </AlertDescription>
+                                    </Box>
+                                </Alert>
+
+                            ) : (
+
+                                <></>
+
+                            )
+
+                        }
+
                     </Center>
 
                     <FormControl mb={5}>
                         <FormLabel><Icon as={BiUser} /> Fullname</FormLabel>
-                        <Input type='text'  required value={FullName} onChange={(event) => setFullName(event.target.value)}/>
+                        <Input type='text' required value={FullName} onChange={(event) => setFullName(event.target.value)} />
                     </FormControl>
 
                     <FormControl mb={5}>
                         <FormLabel><EmailIcon /> Email address</FormLabel>
-                        <Input type='email' required value={Email} onChange={(event) => setEmail(event.target.value)}/>
+                        <Input type='email' required value={Email} onChange={(event) => setEmail(event.target.value)} />
                     </FormControl>
 
                     <FormControl mb={5}>
                         <FormLabel><LockIcon /> Password</FormLabel>
-                        <Input type='password' required value={Password} onChange={(event) => setPassword(event.target.value)}/>
+                        <Input type='password' required value={Password} onChange={(event) => setPassword(event.target.value)} />
                     </FormControl>
 
-                    <Button type="submit" w='100%' leftIcon={<Icon as={BiUserCircle} />} bg='#5279F4' borderRadius={45} border='1px' borderColor='#5279F4'
-                    _hover={{
-                        borderColor: '#5279F4',
-                        backgroundColor: '#000000',
-                        color: '#5279F4'
-                    }}>
-                        Signup
-                    </Button>
+                    {loading ?
+
+                        (
+
+                            <Button isLoading loadingText='Submitting' type="submit" w='100%' bg='#5279F4' borderRadius={45} border='1px' borderColor='#5279F4'
+                                _hover={{
+                                    borderColor: '#5279F4',
+                                    backgroundColor: '#000000',
+                                    color: '#5279F4'
+                                }}>
+                                Signup
+                            </Button>
+
+                        ) : (
+
+                            <Button type="submit" w='100%' leftIcon={<Icon as={BiUserCircle} />} bg='#5279F4' borderRadius={45} border='1px' borderColor='#5279F4'
+                                _hover={{
+                                    borderColor: '#5279F4',
+                                    backgroundColor: '#000000',
+                                    color: '#5279F4'
+                                }}>
+                                Signup
+                            </Button>
+
+                        )
+
+                    }
+
+
 
                 </form>
 
