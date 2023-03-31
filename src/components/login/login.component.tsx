7c4581ca0c993913c7ab91dc6a917b1ab69a5c35
Jhon-crypt <johnoladele690@gmail.com>
2023-03-31 11:32:20 +0100

    Updated login

--- src/components/login/login.component.tsx
+++ src/components/login/login.component.tsx
@@ -5,14 +5,74 @@ import {
     FormControl,
     FormLabel,
     Input,
-    Button
+    Button,
+    AlertIcon,
+    Alert,
+    AlertDescription,
+    Box
 } from '@chakra-ui/react'
 import { Icon } from '@chakra-ui/icons';
 import { BiLogInCircle } from "react-icons/bi";
 import { EmailIcon } from '@chakra-ui/icons';
 import { LockIcon } from '@chakra-ui/icons'
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom';
+import supabase from '../../supabase/supabase';
 
 function Login() {
+
+    let navigate = useNavigate()
+
+    const [Email, setEmail] = useState('')
+
+    const [Password, setPassword] = useState('')
+
+    const [loading, setLoading] = useState(false);
+
+    const [error, setError] = useState(false)
+
+    const handleLogin = async (event: any) => {
+
+       
+        event.preventDefault();
+
+        try {
+
+            setLoading(true)
+
+            const { data, error }: any = await supabase.auth.signInWithPassword({
+                email: `${Email}`,
+                password: `${Password}`,
+            })
+
+            setLoading(false)
+
+            if (error) {
+
+                setError(true)
+
+                console.log(data)
+
+            } else {
+
+                setError(false)
+
+                console.log(error)
+
+                navigate("/App")
+
+
+            }
+
+        } catch (error) {
+
+            console.log(error)
+
+        }
+
+
+
+    }
 
     return (
 
@@ -29,26 +89,73 @@ function Login() {
 
                 <br />
 
-                <form>
+                <form onSubmit={handleLogin}>
+
+                    <Center>
+
+                        {error ?
+
+                            (
+
+                                <Alert status='error' mb={3} color="black" as="b" borderRadius={45} w={300}>
+                                    <AlertIcon />
+                                    <Box>
+                                        <AlertDescription>
+                                            <Center>
+                                                Wrong email or password
+                                            </Center>
+                                        </AlertDescription>
+                                    </Box>
+                                </Alert>
+
+                            ) : (
+
+                                <></>
+
+                            )
+
+                        }
+
+                    </Center>
 
                     <FormControl mb={5}>
                         <FormLabel><EmailIcon /> Email address</FormLabel>
-                        <Input type='email' required/>
+                        <Input type='email' required value={Email} onChange={(event) => setEmail(event.target.value)} />
                     </FormControl>
 
                     <FormControl mb={5}>
                         <FormLabel><LockIcon /> Password</FormLabel>
-                        <Input type='password' required/>
+                        <Input type='password' required value={Password} onChange={(event) => setPassword(event.target.value)} />
                     </FormControl>
 
-                    <Button type="submit" w='100%' leftIcon={<Icon as={BiLogInCircle} />} bg='#5279F4' borderRadius={45} border='1px' borderColor='#5279F4'
-                    _hover={{
-                        borderColor: '#5279F4',
-                        backgroundColor: '#000000',
-                        color: '#5279F4'
-                    }}>
-                        Login
-                    </Button>
+                    {loading ?
+                        (
+
+                            <Button isLoading loadingText='Loggin in' type="submit" w='100%' leftIcon={<Icon as={BiLogInCircle} />} bg='#5279F4' borderRadius={45} border='1px' borderColor='#5279F4'
+                                _hover={{
+                                    borderColor: '#5279F4',
+                                    backgroundColor: '#000000',
+                                    color: '#5279F4'
+                                }}>
+                                Login
+                            </Button>
+
+                        ) : (
+
+                            <Button type="submit" w='100%' leftIcon={<Icon as={BiLogInCircle} />} bg='#5279F4' borderRadius={45} border='1px' borderColor='#5279F4'
+                                _hover={{
+                                    borderColor: '#5279F4',
+                                    backgroundColor: '#000000',
+                                    color: '#5279F4'
+                                }}>
+                                Login
+                            </Button>
+
+                        )
+
+                    }
+
+
 
                 </form>
 
